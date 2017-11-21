ce0b3e25b0b255edef0d4716a0b993d58ecf366f
zqq90 <zqq_90@163.com>
2017-11-21 17:58:16 +0800

    hdata-wit: added a empty function: noop

--- hdata-wit/src/main/java/com/github/stuxuhai/hdata/plugin/wit/Methods.java
+++ hdata-wit/src/main/java/com/github/stuxuhai/hdata/plugin/wit/Methods.java
@@ -1,9 +1,12 @@
 package com.github.stuxuhai.hdata.plugin.wit;
 
 import com.github.stuxuhai.hdata.api.Record;
+import org.febit.wit.Context;
 import org.febit.wit.Engine;
+import org.febit.wit.InternalContext;
 import org.febit.wit.core.NativeFactory;
 import org.febit.wit.global.GlobalManager;
+import org.febit.wit.lang.MethodDeclare;
 import org.febit.wit.util.JavaNativeUtil;
 
 /**
@@ -11,6 +14,16 @@ import org.febit.wit.util.JavaNativeUtil;
  * @author zqq90
  */
 public class Methods implements WitEnginePlugin {
+
+    /**
+     * A empty function, do nothing.
+     */
+    public static final MethodDeclare noop = new MethodDeclare() {
+        @Override
+        public Object invoke(InternalContext context, Object[] args) {
+            return Context.VOID;
+        }
+    };
 
     public static final Record newRecord() {
         return new WitDynamicRecord();
